5ed694ac261a0220bae2833c9a5b2c0883a19528
Mohamed Refaat <eltntawy@gmail.com>
2015-01-18 23:39:01 +0200

    Apply LookAndFeel

--- src/com/chat/controller/Main.java
+++ src/com/chat/controller/Main.java
@@ -1,5 +1,12 @@
 package com.chat.controller;
 
+import de.javasoft.plaf.synthetica.SyntheticaWhiteVisionLookAndFeel;
+import git.eclipse.demo.TestLookAndFeelForm;
+import java.awt.Frame;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
 
     public Main() {
@@ -8,5 +15,27 @@ public class Main {
 
     public static void main(String arg []) {
 	
+        final JFrame frame = new JFrame("Massanger");
+        frame.setSize(800,700);
+        try {
+
+            //UIManager.setLookAndFeel("com.alee.laf.WebLookAndFeel");
+            //UIManager.installLookAndFeel("SeaGlass", "com.seaglasslookandfeel.SeaGlassLookAndFeel");
+            
+            //UIManager.setLookAndFeel(new SyntheticaStandardLookAndFeel());
+            UIManager.setLookAndFeel(new SyntheticaWhiteVisionLookAndFeel());
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                // TODO Auto-generated method stub
+                SwingUtilities.updateComponentTreeUI(frame);
+            }
+        });
     }
 }
--- src/com/chat/view/TempletePanel.java
+++ src/com/chat/view/TempletePanel.java
@@ -27,34 +27,23 @@ public class TempletePanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-
         setMaximumSize(new java.awt.Dimension(300, 800));
         setMinimumSize(new java.awt.Dimension(300, 800));
         setPreferredSize(new java.awt.Dimension(300, 800));
-
-        jLabel1.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(97, 97, 97)
-                .addComponent(jLabel1)
-                .addContainerGap(152, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(115, 115, 115)
-                .addComponent(jLabel1)
-                .addContainerGap(670, Short.MAX_VALUE))
+            .addGap(0, 800, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
